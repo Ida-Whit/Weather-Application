8418d00e3e474172c1f91215ee2c35f86f1f545e
Ida Whitcomb <idawhitcomb@gmail.com>
2023-11-10 10:15:02 -0500

    Adding HTML framework and some JavaScript functionality

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+// Open Weather API Key 1b6d3059dfe3401cd669baece5f028da
+
+// Call to Open Weather API: https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+// Call to Open Weather API for Lat,Long: http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+const userStorage = localStorage.getItem("location") || []
+
+$("#btn").on("click", function(){
+  $(this).val('')
+  //let userInput = $("#input").val()
+  //userStorage.push(userInput)
+  //localStorage.setItem("location", userInput)
+  $("#past-cities").append(
+    '<li>userInput</li>')
+})
+
+
+//fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + {city name},{state code},{country code} + "&limit=5&appid=1b6d3059dfe3401cd669baece5f028da")
+  //  .then (let coordinates = )
+
+
+ /* $("button").click(function() {
+    $(".append").append(
+'<div class="added">New HTML element added</div>');
+} */
