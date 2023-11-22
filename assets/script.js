7f0a52541bf40025f644d22f6175e19849c1b202
Ida Whitcomb <idawhitcomb@gmail.com>
2023-11-22 16:27:32 -0500

    Added local storage and fetch call to JavaScript

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,74 @@
-// Open Weather API Key 1b6d3059dfe3401cd669baece5f028da
+// Icon URL https://openweathermap.org/img/wn/10d@2x.png 
 
-// Call to Open Weather API: https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+$(function () {
+  //letiables
+  const userStorage = JSON.parse(localStorage.getItem("location")) || []
+  const savedSearches = $(".savedSearches")
+  const APIKey = "1b6d3059dfe3401cd669baece5f028da"
 
-// Call to Open Weather API for Lat,Long: http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-const userStorage = localStorage.getItem("location") || []
+  // Pull from local Storage and display past searches on page load
+  if (userStorage != null) {
+    for (let i = 0; i < userStorage.length; i++) {
+      savedSearches.append($("<button></button>").text(userStorage[i]))
+    }
+  }
 
-$("#btn").on("click", function(){
-  $(this).val('')
-  console.log("Hello")
-  //let userInput = $("#input").val()
-  //userStorage.push(userInput)
-  //localStorage.setItem("location", userInput)
-  $("#past-cities").append(
-    "<li></li>")
-})
+  // Listen for search button click and fire function to add a button and run the API search for latitude and logitude
+  $(".btn").on("click", function (event) {
+    event.preventDefault();
+    addSearched();
+    findWeather();
+  })
+
+  function addSearched() {
+    let userInput = $(".form-control").val()
+    userStorage.push(userInput)
+    localStorage.setItem("location", JSON.stringify(userStorage))
+    savedSearches.append($("<button></button>").text(userInput))
+  }
+
+  function findWeather() {
+    let userInput = $(".form-control").val()
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${userInput}&units=imperial&appid=${APIKey}`)
+      .then(function (response) {
+        return response.json()
+      })
+      .then(function (data) {
+        console.log(data)  
+        const city = document.getElementById("currentCity")
+        for (let i = 0; i<5; i++) {
+          const date = document.createElement("li")
+          //const weatherConditions = document.createElement('li');
+          const temperature = document.createElement('li');
+          const humidity = document.createElement('li');
+          const windSpeed = document.createElement("li");
+  
+          date.innerHTML = "Date: " + data.list[i].dt_txt
+          //weatherConditions.innerHTML = data.list[i].weather;
+          temperature.innerHTML = "Temperature: " + data.list[i].main.temp;
+          humidity.innerHTML = "Humidity: " + data.list[i].main.humidity;
+          windSpeed.innerHTML = "Wind Speed: " + data.list[i].wind.speed;
+        
+          city.appendChild(date)
+          //city.appendChild(weatherConditions)
+          city.appendChild(temperature)
+          city.appendChild(humidity)
+          city.appendChild(windSpeed)
+        }
+      });
+  }
 
 
-//fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + {city name},{state code},{country code} + "&limit=5&appid=1b6d3059dfe3401cd669baece5f028da")
-  //  .then (let coordinates = )
+  // Listen for clean button click and clear local storage and recent searches buttons
+  $("#clear-btn").on("click", function () {
+    localStorage.clear();
+    savedSearches.children().detach()
+  })
 
 
- /* $("button").click(function() {
-    $(".append").append(
-'<div class="added">New HTML element added</div>');
-} */
+
+  /* $("button").click(function() {
+     $(".append").append(
+ '<div class="added">New HTML element added</div>');
+ } */
+});
