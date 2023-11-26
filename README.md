836794799aeb21e22b751feeda19caef2978ea88
Ida Whitcomb <idawhitcomb@gmail.com>
2023-11-26 13:36:50 -0500

    Added README and added styling in CSS

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# Weather-Application
+## Name
+Weather Application
+
+## Description
+This weather application allows a user to search any location within the United States and get current weather conditions as well as a 5-day forecast for that location. Once a location is searched, the program then add a button of that location to the previously searched list. This button can then be clicked at any time to return to the weather search results for that location.
+
+## Badges
+N/A
+
+## Visuals
+Depending on what you are making, it can be a good idea to include screenshots or even a video (you'll frequently see GIFs rather than actual videos). Tools like ttygif can help, but check out Asciinema for a more sophisticated method.
+
+## Installation
+N/A
+
+## Usage
+In order to use this application, the user only needs to enter the name of a town or city in the first search box and the two letter abbreviation of the state in the second search box and click search. The program then uses the Open Weather API to find that locations latitude and longitude coordinates which then plug into a current weather search and a 5-day weather search. Once this search has been completed, the program then displays these results for the user to see.
+
+## Support
+N/A
+
+## Roadmap
+N/A
+
+## Authors and acknowledgment
+This program was written and tested by myself, Ida Whitcomb, with help from the AskBCS learning assitants.
+
+This program uses the API program from Open Weather Map (https://openweathermap.org/current)
--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,6 @@
 //https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 //https://api.openweathermap.org/data/2.5/forecast?q={city name},{state code}&appid={API key}
 
-// Icon URL https://openweathermap.org/img/wn/10d@2x.png 
 
 //`https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png`
 
@@ -10,7 +9,12 @@ $(function () {
   const userStorage = JSON.parse(localStorage.getItem("location")) || []
   const savedSearches = $(".savedSearches")
   const APIKey = "1b6d3059dfe3401cd669baece5f028da"
-  //let iconurl = "http://openweathermap.org/img/wn/" + iconcode + "@2x.png";
+  const icon = document.createElement("img")
+  const current = document.getElementById("current")
+  const temperatureC = document.createElement('ul');
+  const humidity = document.createElement('li');
+  const windSpeed = document.createElement("li");
+  const name = document.createElement("h1")
 
   // Pull from local Storage and display past searches on page load
   if (userStorage != null) {
@@ -18,6 +22,12 @@ $(function () {
       savedSearches.append($("<button class = saved></button>").text(userStorage[i]))
     }
   }
+
+   // Listen for clean button click and clear local storage and recent searches buttons
+   $("#clear-btn").on("click", function () {
+    localStorage.clear();
+    savedSearches.children().detach()
+  })
 
   // Listen for search button click and fire function to add a button and run the API search for forecast
   $(".btn").on("click", function (event) {
@@ -27,106 +37,100 @@ $(function () {
   })
 
   function addSearched() {
-    let userInput = $(".form-control").val()
+    const userCity = $("#city").val()
+    const userState = $("#state").val()
+    userInput = userCity + ", " + userState
     userStorage.push(userInput)
     localStorage.setItem("location", JSON.stringify(userStorage))
     savedSearches.append($("<button class = saved></button>").text(userInput))
   }
 
-    //Run API search for lat and long and then current weather
-    function currentWeather () {
-      let userCity = $("#city").val()
-      let userState= $("#state").val()
-      fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${userCity},${userState},us&appid=${APIKey}`)
+  //Run API search for lat and long and then current weather
+  function currentWeather() {
+    let userCity = $("#city").val()
+    let userState = $("#state").val()
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${userCity},${userState},us&appid=${APIKey}`)
       .then(function (response) {
         return response.json()
       })
       .then(function (data) {
         const latitude = data.city.coord.lat
         const longitude = data.city.coord.lon
-    
-        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${APIKey}`)
-        .then(function(response){
-          return response.json()
-        })
-        .then(function (data) {
-          console.log(data)
-          const iconcode = data.weather[0].icon
-          const iconurl = "http://openweathermap.org/img/wn/" + iconcode + "@2x.png";
-          const icon = document.createElement("img")
-          const current = document.getElementById("current")
-          const temperature = document.createElement('ul');
-          const humidity = document.createElement('li');
-          const windSpeed = document.createElement("li");
-    
-          temperature.innerHTML = "Temperature: " + data.main.temp + '\u00B0' + "F";
-          humidity.innerHTML = "Humidity: " + data.main.humidity + "%";
-          windSpeed.innerHTML = "Wind Speed: " + data.wind.speed + "mph";
-    
-          icon.setAttribute("src", iconurl)
-    
-          current.appendChild(temperature)
-          temperature.appendChild(humidity)
-          temperature.appendChild(windSpeed)
-          current.appendChild(icon)
-        })
-      })
-      forecast()
-    };
 
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${APIKey}`)
+          .then(function (response) {
+            return response.json()
+          })
+          .then(function (data) {
+            const iconcode = data.weather[0].icon
+            const iconurl = "http://openweathermap.org/img/wn/" + iconcode + "@2x.png";
+
+            name.innerHTML = userCity + ", " + userState;
+            temperatureC.innerHTML = "Temperature: " + data.main.temp + '\u00B0' + "F";
+            humidity.innerHTML = "Humidity: " + data.main.humidity + "%";
+            windSpeed.innerHTML = "Wind Speed: " + data.wind.speed + "mph";
+
+            icon.setAttribute("src", iconurl)
+            current.appendChild(name)
+            name.appendChild(icon)
+            name.appendChild(temperatureC)
+            name.appendChild(humidity)
+            name.appendChild(windSpeed)
+          })
+      })
+    forecast()
+  };
+
+  //Run API for a 5-day forecast
   function forecast() {
     let userCity = $("#city").val()
-    let userState= $("#state").val()
-  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${userCity},${userState},us&appid=${APIKey}`)
-  .then(function (response) {
-    return response.json()
-  })
-  .then(function (data) {
-    const latitude = data.city.coord.lat
-    const longitude = data.city.coord.lon
+    let userState = $("#state").val()
 
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=imperial&appid=${APIKey}`)
-    .then(function(response){
-      return response.json()
-    })  
-    .then(function (data) {
-        const city = document.getElementById("forecast")
-        console.log(data)
-        for (let i = 0; i < 40; i = i + 8) {
-          let iconcode = data.list[i].weather[0].icon;
-          let iconurl = "http://openweathermap.org/img/wn/" + iconcode + "@2x.png";
-          const date = document.createElement("ul")
-          const temperature = document.createElement('li');
-          const humidity = document.createElement('li');
-          const windSpeed = document.createElement("li");
-          const icon = document.createElement("img")
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${userCity},${userState},us&appid=${APIKey}`)
+      .then(function (response) {
+        return response.json()
+      })
+      .then(function (data) {
+        const latitude = data.city.coord.lat
+        const longitude = data.city.coord.lon
 
-          icon.setAttribute("src", iconurl)
-          date.innerHTML = "Date: " + data.list[i].dt_txt
-          temperature.innerHTML = "Temperature: " + data.list[i].main.temp + '\u00B0' + "F";
-          humidity.innerHTML = "Humidity: " + data.list[i].main.humidity + "%";
-          windSpeed.innerHTML = "Wind Speed: " + data.list[i].wind.speed + "mph";
-          
-          city.appendChild(date)
-          date.appendChild(icon)
-          date.appendChild(temperature)
-          date.appendChild(humidity)
-          date.appendChild(windSpeed)
-        }
+        fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=imperial&appid=${APIKey}`)
+          .then(function (response) {
+            return response.json()
+          })
+          .then(function (data) {
+            const city = document.getElementById("forecast")
+            for (let i = 0; i < 40; i = i + 8) {
+              let iconcode = data.list[i].weather[0].icon;
+              let iconurl = "http://openweathermap.org/img/wn/" + iconcode + "@2x.png";
+              const date = document.createElement("ul")
+              const temperature = document.createElement('li');
+              const humidity = document.createElement('li');
+              const windSpeed = document.createElement("li");
+              const icon = document.createElement("img")
+
+              date.innerHTML = "Date: " + data.list[i].dt_txt
+              temperature.innerHTML = "Temperature: " + data.list[i].main.temp + '\u00B0' + "F";
+              humidity.innerHTML = "Humidity: " + data.list[i].main.humidity + "%";
+              windSpeed.innerHTML = "Wind Speed: " + data.list[i].wind.speed + "mph";
+
+              icon.setAttribute("src", iconurl)
+              
+              city.appendChild(date)
+              date.appendChild(icon)
+              date.appendChild(temperature)
+              date.appendChild(humidity)
+              date.appendChild(windSpeed)
+            }
+          });
       });
-    });
-
-  // Listen for clean button click and clear local storage and recent searches buttons
-  $("#clear-btn").on("click", function () {
-    localStorage.clear();
-    savedSearches.children().detach()
-  })
-};
+  };
 });
-  /*//Listen for button click and take you to the search results of that button
-  $(".savedSearches").on("click", ".saved", function(event) {
-    console.log(this)  
-    event.preventDefault();
-      $("#input").val(this.textContent)
-      $(".btn").click()   
-  })*/
+
+/*//Listen for button click and take you to the search results of that button
+$(".savedSearches").on("click", ".saved", function(event) {
+  console.log(this)  
+  event.preventDefault();
+    $("#input").val(this.textContent)
+    $(".btn").click()   
+})*/
